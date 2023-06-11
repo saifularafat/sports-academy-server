63d69956ee1e1ef23d0405c0f9b3f078e4fbc021
saifularafat <saifularafat.info@gmail.com>
2023-06-11 16:57:11 +0600

    users role and user setUp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -31,12 +31,16 @@ async function run() {
     try {
 
         const usersCollection = client.db('sportsAcademyDB').collection('users')
-        const coachCollection = client.db('sportsAcademyDB').collection('coach')
-        const sportsCollection = client.db('sportsAcademyDB').collection('sports')
+        const instructorsCollection = client.db('sportsAcademyDB').collection('coach')
+        // const classesCollection = client.db('sportsAcademyDB').collection('classes')
         const admissionsCollection = client.db('sportsAcademyDB').collection('admissions')
 
-        // !!!! USERS
-        app.put('/users', async (req, res) => {
+        // !!!! USERS api 
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
@@ -47,10 +51,32 @@ async function run() {
             const result = await usersCollection.insertOne(user)
             res.send(result);
         })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
 
-        // !!!! COACH
-        app.get('/coach', async (req, res) => {
-            const result = await coachCollection.find().toArray();
+        // !!!! COACH api
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorsCollection.find().toArray();
             res.send(result)
         })
 
